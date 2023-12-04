9e822117e98ce71f7b46446d65fc646086cbda4b
wellrod <taliev@digital-spirit.ru>
2023-12-04 23:34:39 +0400

    Убирает not null для пароля

--- src/main/java/miniapp/timetracker/model/UserAuth.java
+++ src/main/java/miniapp/timetracker/model/UserAuth.java
@@ -11,7 +11,6 @@ public class UserAuth {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
-    @Column(nullable = false)
     private String password;
     @Column(nullable = false)
     private String login;
